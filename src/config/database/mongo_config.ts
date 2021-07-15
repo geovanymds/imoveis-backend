46eda02f1e12f682b46937ba25e68bb78a926185
Geovany Mendes <geovany.mendes@mjv.com.br>
2021-07-15 18:56:19 -0300

    [FIX] Ajusta mongo para execuçao local

--- src/config/database/mongo_config.ts
+++ src/config/database/mongo_config.ts
@@ -1,4 +1,7 @@
-export const uri = <string>process.env.DB_URI;
+export const uri =
+  process.env.ENVIROMENT === "DOCKER"
+    ? <string>process.env.DB_URI
+    : <string>process.env.DB_URI_LOCALHOST;
 
 export const config = {
   db: process.env.DB_NAME,
