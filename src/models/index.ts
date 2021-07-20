4c0d918cc3880cad01ae54eda3ac76ceb11be934
Geovany Mendes <geovany.mendes@mjv.com.br>
2021-07-20 19:58:18 -0300

    [ADD] CRU Corretor

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 import { Imovel, ImovelModel } from "./Imovel";
 import { ImageModel } from "./Image";
+import { Corretor, CorretorModel } from "./Corretor"
 
-export { Imovel, ImovelModel, ImageModel };
+export { Imovel, ImovelModel, ImageModel, Corretor, CorretorModel };
+
